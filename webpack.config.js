f50e7c8caaf11b39a154e28ac2eb24c4102773bc
Treighton <treighton@pagedesigngroup.com>
2019-01-23 21:49:15 -0800

    fixing shit

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundledup.js"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundledup.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
           plugins: [
-            "react-hot-loader/babel",
-            "@babel/plugin-proposal-class-properties"
+            'react-hot-loader/babel',
+            '@babel/plugin-proposal-class-properties'
           ]
         }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
+      template: './src/index.html'
+    }),
+    new BundleAnalyzerPlugin()
   ],
   devServer: {
     port: 8000
   }
-};
+}
