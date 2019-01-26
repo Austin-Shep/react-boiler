e033ccceabff0c390d5b7c467fa780a5a1851e1b
Treighton <treighton@pagedesigngroup.com>
2019-01-26 13:33:46 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { hot } from "react-hot-loader";
-import greeter from "./components/greeter";
-import "./styles/index.css";
+import React from 'react'
+import { hot } from 'react-hot-loader'
+//import greeter from "./components/greeter";
+import './styles/index.css'
 
 class App extends React.Component {
   state = {
     counter: 0
-  };
+  }
   render() {
     return (
       <div>
         <h1>Count!: {this.state.counter}</h1>
+        <img src="https://via.placeholder.com/150" />
         <button
           onClick={() =>
             this.setState(state => ({ counter: state.counter + 1 }))
@@ -19,8 +20,8 @@ class App extends React.Component {
           +
         </button>
       </div>
-    );
+    )
   }
 }
-
-export default hot(module)(App);
+// eslint-disable-next-line
+export default hot(module)(App)
